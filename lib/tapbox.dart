d05967de05b9b07f404c8bb0ff128945d2808e44
nanyanen87 <nan.hanaoka@gmail.com>
2023-03-05 19:46:24 +0900

    interactiveなコンポーネントtutorial

---
+++ lib/tapbox.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+
+class Tapbox extends StatefulWidget {
+  const Tapbox({super.key});
+
+  @override
+  State<Tapbox> createState() => _TapboxWidgetState();
+}
+class _TapboxWidgetState extends State<Tapbox>{
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          TapboxA(),
+          TapboxB(
+            active: _active,
+            onChanged: _handleTapboxChanged,
+          ),
+          TapboxC(
+            active: _active,
+            onChanged: _handleTapboxChanged,
+          )
+        ]
+    );
+  }
+}
+
+class TapboxA extends StatefulWidget {
+  const TapboxA({super.key});
+
+  @override
+  State<TapboxA> createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+        child: Center(
+          child: Text(
+            _active ? 'Active' : 'Inactive',
+            style: const TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// こいつらはstateを持っていない
+class TapboxB extends StatelessWidget {
+  const TapboxB({
+  super.key,
+  this.active = false,
+  required this.onChanged,
+  });
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: const TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TapboxC extends StatefulWidget {
+  const TapboxC({
+  super.key,
+  this.active = false,
+  required this.onChanged,
+  });
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  State<TapboxC> createState() => _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This example adds a green border on tap down.
+    // On tap up, the square changes to the opposite state.
+    return GestureDetector(
+      onTapDown: _handleTapDown, // Handle the tap events in the order that
+      onTapUp: _handleTapUp, // they occur: down, up, tap, cancel
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: Container(
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight
+              ? Border.all(
+            color: Colors.teal[700]!,
+            width: 10.0,
+          )
+              : null,
+        ),
+        child: Center(
+          child: Text(widget.active ? 'Active' : 'Inactive',
+              style: const TextStyle(fontSize: 32.0, color: Colors.white)),
+        ),
+      ),
+    );
+  }
+}
